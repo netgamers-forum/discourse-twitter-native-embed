908e0e8cf2f70723f3a70175dcc86326f7962d9e
hatsoptional <160878611+hatsoptional@users.noreply.github.com>
2025-02-05 22:02:09 +0100

    Rimozione embed, teniamo solo link xcancel

--- javascripts/discourse/initializers/discourse-twitter-native-embed.js
+++ javascripts/discourse/initializers/discourse-twitter-native-embed.js
@@ -7,7 +7,7 @@ export default {
     name: "discourse-twitter-native-embed-xcancel",
     initialize() {
         withPluginApi("1.0.0", api => {
-
+/*
             function getTwitterScript() {
                 var scriptnode = document.createElement('script');
                 scriptnode.setAttribute("async", "");
@@ -15,32 +15,34 @@ export default {
                 scriptnode.setAttribute("charset", "utf-8");
                 document.head.appendChild(scriptnode);
             }
+*/
             api.decorateCookedElement((el, helper) => {
-                let hasQuote = false;
+//                let hasQuote = false;
                 for (const the_musks_fxxking_url of ["twitter.com", "x.com"]) {
                     for (const aa of el.querySelectorAll(`a.onebox[href^="https://${the_musks_fxxking_url}/"][href*=status]`)) {
-                        const twitter_blockquoue = document.createElement("blockquote");
-                        twitter_blockquoue.setAttribute("style", "display: none");
-                        twitter_blockquoue.classList?.add("twitter-tweet");
-                        const aaa = document.createElement("a");
-                        aaa.setAttribute("href", aa.href.replaceAll("https://x.com", "https://twitter.com"));
-                        aaa.setAttribute("rel", "no-follow");
-                        twitter_blockquoue.appendChild(aaa);
-                        aa.appendChild(twitter_blockquoue);
-                        const xcancel_link = document.createElement("a");
+//                        const twitter_blockquoue = document.createElement("blockquote");
+//                        twitter_blockquoue.setAttribute("style", "display: none");
+//                        twitter_blockquoue.classList?.add("twitter-tweet");
+//                        const aaa = document.createElement("a");
+//                        aaa.setAttribute("href", aa.href.replaceAll("https://x.com", "https://twitter.com"));
+//                        aaa.setAttribute("rel", "no-follow");
+//                        twitter_blockquoue.appendChild(aaa);
+//                        aa.appendChild(twitter_blockquoue);
+//                        const xcancel_link = document.createElement("a");
+                        const xcancel_link = aa;
                         xcancel_link.setAttribute("href", aa.href.replaceAll(/^https:\/\/.+\.com/gi, "https://xcancel.com"));
-                        xcancel_link.text = "Apri in Xcancel";
+//                        xcancel_link.text = "Apri in Xcancel";
                         xcancel_link.setAttribute("rel", "no-follow");
                         aa.appendChild(xcancel_link);
                     }
                 }
-                for (const quote of el.getElementsByTagName("blockquote")) {
-                    if (quote.querySelector(`blockquote a[href^="https://twitter.com/"]`)) {
-                        quote.classList?.add("twitter-tweet");
-                        hasQuote = true;
-                    }
-                }
-                if (hasQuote) getTwitterScript();
+//                for (const quote of el.getElementsByTagName("blockquote")) {
+//                    if (quote.querySelector(`blockquote a[href^="https://twitter.com/"]`)) {
+//                        quote.classList?.add("twitter-tweet");
+//                        hasQuote = true;
+//                    }
+//               }
+//                if (hasQuote) getTwitterScript();
             }, {
                 id: "discourse-twitter-native-embed-xcancel",
                 afterAdopt: true,
