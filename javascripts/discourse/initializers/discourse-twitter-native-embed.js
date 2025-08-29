de107d0b8de961b0ab6e4950a149cffd00b2ecfb
char-w <hellslayer.char@gmail.com>
2025-08-29 19:25:16 +0200

    Next

--- javascripts/discourse/initializers/discourse-twitter-native-embed.js
+++ javascripts/discourse/initializers/discourse-twitter-native-embed.js
@@ -21,7 +21,7 @@ export default {
                 for (const the_musks_fxxking_url of ["twitter.com", "x.com"]) {
                     for (const aa of el.querySelectorAll(`aside.onebox[data-onebox-src^="https://${the_musks_fxxking_url}/"][data-onebox-src*=status]`)) {
                         const xcancel_link = aa;
-                        xcancel_link.setAttribute("href", aa.getAttribute("data-onebox-src").replaceAll(/^https:\/\/.+\.com/gi, "https://xcancel.com"));
+                        xcancel_link.setAttribute("data-onebox-src", aa.getAttribute("data-onebox-src").replaceAll(/^https:\/\/.+\.com/gi, "https://xcancel.com"));
                         xcancel_link.textContent = aa.textContent.replaceAll(/^https:\/\/.+\.com/gi, "https://xcancel.com")
                         xcancel_link.setAttribute("rel", "no-follow");
                     }
